97f0ba39050bd51f69a37cb98e63b3e264d21e93
Craig Magina <cmagina@redhat.com>
2025-06-02 16:51:05 -0400

    Fixes to run axpy and dot kernels with triton-cpu
    Signed-off-by: Craig Magina <cmagina@redhat.com>

--- tests/test_axpy.py
+++ tests/test_axpy.py
@@ -4,12 +4,13 @@ import triton
 from triton.testing import assert_close
 
 
-DEVICE = "cuda"
+DEVICE = triton.runtime.driver.active.get_current_target().backend
+
 
 # TODO either make this shared or make a shared rtol function
 def is_hip_mi200():
-    target = triton.runtime.driver.active.get_current_target()
-    return target.backend == 'hip' and target.arch == 'gfx90a'
+    return DEVICE == "hip" and triton.runtime.driver.active.get_current_target().arch == "gfx90a"
+
 
 def test_axpy():
     size = 98432
@@ -22,6 +23,6 @@ def test_axpy():
 
     triton_output = tb.axpy(x, y, alpha)
     torch_output = (alpha * x) + y
-    
+
     rtol = 1e-2 if is_hip_mi200() else 0
     assert_close(triton_output, torch_output, rtol=rtol)
