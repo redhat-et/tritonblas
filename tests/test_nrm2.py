5b6f0f6ed502199f5b93f2ef95524de0d8bda953
kyolebu <kyu@redhat.com>
2025-06-10 20:55:32 +0000

    updated nrm2 kernel and test

--- tests/test_nrm2.py
+++ tests/test_nrm2.py
@@ -2,17 +2,21 @@ import torch
 import triton
 from triton.testing import assert_close
 import tritonblas as tb
+from .utils import get_rtol
 
 
-DEVICE = "cuda"
+DEVICE = triton.runtime.driver.active.get_current_target().backend
+
 
 def test_nrm2():
     size = 98432
 
     torch.manual_seed(0)
-    x = torch.randn(size, device=DEVICE, dtype=torch.float16)
+    dtype = torch.float32
+    x = torch.randn(size, device=DEVICE, dtype=dtype)
 
     triton_output = tb.nrm2(x)
-    torch.output = torch.linalg.norm(x)
+    torch_output = torch.linalg.norm(x)
     
+    rtol = get_rtol()
     assert_close(triton_output, torch_output, rtol=rtol)
