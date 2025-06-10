416ba4524904930a9b95b9fe57bc375d74a6a486
kyolebu <kyu@redhat.com>
2025-06-10 15:58:12 +0000

    nrm2 kernel

--- tritonblas/level1/nrm2.py
+++ tritonblas/level1/nrm2.py
@@ -2,8 +2,6 @@ import torch
 import triton
 import triton.language as tl
 
-def is_cuda():
-    return triton.runtime.driver.active.get_current_target().backend == "cuda"
 
 def get_autotune_config():
     return [
@@ -48,4 +46,35 @@ def get_autotune_config():
 
 @triton.jit
 def nrm2_kernel(
-)
+    x_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    
+    partial = tl.sum(x**2)
+    total = tl.reduce(partial, axis=0, op=tl.sum)
+    output = tl.sqrt(total)
+
+    tl.store(output_ptr, output)
+
+
+def nrm2(x: torch.Tensor, y: torch.Tensor):
+    output = torch.Tensor()
+    assert x.device == y.device and x.device == output.device
+    n_elements = output.numel()
+
+    def grid(META): return (triton.cdiv(n_elements, META['BLOCK_SIZE']), )
+
+    nrm2_kernel[grid](
+        x,
+        output,
+        n_elements
+    )
+    
+    return output
