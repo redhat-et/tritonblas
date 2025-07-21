2d22115824cd488441b325129a92c18a3b861ad9
kyolebu <kyu@redhat.com>
2025-07-21 14:25:39 +0000

    Full GEMV routine

--- benchmarks/benchmark_axpy.py
+++ benchmarks/benchmark_axpy.py
@@ -6,7 +6,7 @@ from utils import add_tritonblas_lib
 add_tritonblas_lib()
 import tritonblas as tb
 
-DEVICE = "cuda"
+DEVICE = torch.accelerator.current_accelerator()
 
 
 @triton.testing.perf_report(
--- tests/test_gemv.py
+++ tests/test_gemv.py
@@ -1,10 +1,8 @@
 import torch
 import triton
+import tritonblas as tb
 from triton.testing import assert_close
 from .utils import get_rtol
-
-# Import the gemv function directly since it's not exported in the main module yet
-from tritonblas.level2.gemv import gemv
 
 
 DEVICE = triton.runtime.driver.active.get_current_target().backend
@@ -17,9 +15,12 @@ def test_gemv():
     dtype = torch.float32
     a = torch.randn(m, n, device=DEVICE, dtype=dtype)
     x = torch.randn(n, device=DEVICE, dtype=dtype)
+    y = torch.randn(m, device=DEVICE, dtype=dtype)
+    alpha = 0.42
+    beta = 10
 
-    triton_output = gemv(a, x)
-    torch_output = torch.matmul(a, x)
+    triton_output = tb.gemv(a, x, y, alpha, beta)
+    torch_output = alpha * torch.mv(a, x) + beta * y
 
     rtol = get_rtol()
     assert_close(triton_output, torch_output, rtol=rtol)
